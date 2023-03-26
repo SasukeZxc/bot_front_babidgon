4f15af33ce2c9b98fdf07a719f2ac75df2bf0ce5
SasukeZxc <krylovmaksim603@gmail.com>
2023-03-26 12:03:43 +0500

    add from, button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import {Routes, Route} from "react-router-dom";
 import {Header} from './components/Header/Header';
 import {ProductList} from './components/ProductList/ProductList';
 import {Form} from './components/Form/Form';
-const tg = window.Telegram.WebApp;
- 
-
+import { useTelegram } from './hooks/useTelegram';
 function App() {
+  const {onToggleButton, tg} =useTelegram();
   useEffect(()=>{
     tg.ready()
   },[])
@@ -18,13 +17,13 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h1>It's work</h1>
+
         <Header/>
         <Routes>
           <Route index element = {<ProductList/>}/>
           <Route path = {'/form'} element={<Form/>}/>
         </Routes>
-        <button onClick = {closeEvent} className="btn">Закрыть</button>
+        
       </div>
     </div>
   );
